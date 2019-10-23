e6504989b3e7875663ac90adb69c31f4d93a71af
SELF-T-YY <1097166568@qq.com>
2019-10-23 22:50:28 +0800

    selftyy20191023

--- js/force_gai2.js
+++ js/force_gai2.js
@@ -1,193 +1,190 @@
 function drawforce(data){
-    var nodes = [];
-    var links = [];
-    var force_width = 775;
-    var force_height = 509;
-    var circle_Color = 0x3A435E;
-    var line_Color = 0xc6c6c6;
-    d3.json("/data/force_data_gai.json", function populate(datas){
-        for(var key in datas){
-            var node = {};
-            node["id"] = key;
-            node["x"] = datas[key]["x"];
-            node["y"] = datas[key]["y"];
-            nodes.push(node);
+    d3.csv("/data/oregonf_community.csv", function(data_community)
+    {
+        var node_community = [];
+        var node_json = {}
+        for(var node in data_community)
+        {
+            if(data_community[node][1] in node_json)
+                node_json[data_community[node][1]].push(data_community[node][0]);
+            else
+                node_json[data_community[node][1]] = [data_community[node][0]]
+            // console.log(data_community[node])
         }
-        console.log(nodes);
+        console.log(node_json)
 
-        d3.csv("/data/oregonf.csv",function(error,csvdata){
 
-            for(var i=0;i<csvdata.length;i++){
-                var data = {};
-                var a1 = csvdata[i].source;
-                var a2 = csvdata[i].target;
-                data["source"] = a1;
-                data["target"] = a2;
-                links.push(data);
-            }
-            let app = new PIXI.Application({
-                width:force_width,
-                height:force_height,
-                antialias: true,
-                resolution : 1,
-            });
-
-            console.log(nodes);
-
-            document.querySelector('#Centre_2').appendChild(app.view);
-            app.renderer.backgroundColor = 0xffffff;
-            const lines = new PIXI.Graphics();
-            for(var i = 0 ; i < links.length ; i++){
-                lines.lineStyle(0.4,line_Color,1);
-                lines.moveTo(datas[links[i].source].x,datas[links[i].source].y);
-                lines.lineTo(datas[links[i].target].x,datas[links[i].target].y);
-            }
-            app.stage.addChild(lines);
-            console.log(nodes);
-            const circles = new PIXI.Graphics();
+        var nodes = [];
+        var links = [];
+        var force_width = 775;
+        var force_height = 509;
+        var circle_Color = 0x3A435E;
+        var line_Color = 0xc6c6c6;
+        d3.json("/data/force_data_gai.json", function populate(datas){
             for(var key in datas){
-                circles.beginFill(circle_Color);
-                circles.drawCircle(datas[key].x,datas[key].y,5);
-                // circles.drawCircle(nodes[i].x,nodes[i].y,1.5);
-                circles.endFill();
+                var node = {};
+                node["id"] = key;
+                node["x"] = datas[key]["x"];
+                node["y"] = datas[key]["y"];
+                nodes.push(node);
             }
-        
-            if (window.addEventListener)//FF,火狐浏览器会识别该方法
-                window.addEventListener('DOMMouseScroll', wheel, false);
-            window.onmousewheel = document.onmousewheel = wheel; //W3C
-            //统一处理滚轮滚动事件
-            function wheel(event){
-                var delta = 0;
-                if (!event) event = window.event;
-                if (event.wheelDelta) {//IE、chrome浏览器使用的是wheelDelta，并且值为“正负120”
-                    delta = event.wheelDelta/120; 
-                    if (window.opera) delta = -delta;//因为IE、chrome等向下滚动是负值，FF是正值，为了处理一致性，在此取反处理
-                } else if (event.detail) {//FF浏览器使用的是detail,其值为“正负3”
-                    delta = -event.detail/3;
+            console.log(nodes);
+
+            d3.csv("/data/oregonf.csv",function(error,csvdata){
+
+                for(var i=0;i<csvdata.length;i++){
+                    var data = {};
+                    var a1 = csvdata[i].source;
+                    var a2 = csvdata[i].target;
+                    data["source"] = a1;
+                    data["target"] = a2;
+                    links.push(data);
                 }
-                if (delta)
-                    handle(delta);
-            }
-            //上下滚动时的具体处理函数
-            function handle(delta) {
-                    if (delta <0){//向下滚动
-                        circles.scale.x *= 1.2;
-                        circles.scale.y *= 1.2;
-                        lines.scale.x *= 1.2;
-                        lines.scale.y *= 1.2;
-                    }else{//向上滚动 
-                        circles.scale.x /= 1.2;
-                        circles.scale.y /= 1.2;
-                        lines.scale.x /= 1.2;
-                        lines.scale.y /= 1.2;
+                let app = new PIXI.Application({
+                    width:force_width,
+                    height:force_height,
+                    antialias: true,
+                    resolution : 1,
+                });
+
+                console.log(nodes);
+
+                document.querySelector('#Centre_2').appendChild(app.view);
+                app.renderer.backgroundColor = 0xffffff;
+                const lines = new PIXI.Graphics();
+                for(var i = 0 ; i < links.length ; i++){
+                    lines.lineStyle(0.4,line_Color,1);
+                    lines.moveTo(datas[links[i].source].x,datas[links[i].source].y);
+                    lines.lineTo(datas[links[i].target].x,datas[links[i].target].y);
+                }
+                app.stage.addChild(lines);
+                console.log(nodes);
+                const circles = new PIXI.Graphics();
+                for(var key in datas){
+                    circles.beginFill(circle_Color);
+                    circles.drawCircle(datas[key].x,datas[key].y,5);
+                    // circles.drawCircle(nodes[i].x,nodes[i].y,1.5);
+                    circles.endFill();
+                }
+            
+                if (window.addEventListener)//FF,火狐浏览器会识别该方法
+                    window.addEventListener('DOMMouseScroll', wheel, false);
+                window.onmousewheel = document.onmousewheel = wheel; //W3C
+                //统一处理滚轮滚动事件
+                function wheel(event){
+                    var delta = 0;
+                    if (!event) event = window.event;
+                    if (event.wheelDelta) {//IE、chrome浏览器使用的是wheelDelta，并且值为“正负120”
+                        delta = event.wheelDelta/120; 
+                        if (window.opera) delta = -delta;//因为IE、chrome等向下滚动是负值，FF是正值，为了处理一致性，在此取反处理
+                    } else if (event.detail) {//FF浏览器使用的是detail,其值为“正负3”
+                        delta = -event.detail/3;
                     }
-            }
-            
-
-            circles.buttonMode = true;
-            circles.interactive = true;
-            
-            circles
-                .on('pointerdown', onDragStart)
-                .on('pointerup', onDragEnd)
-                .on('pointerupoutside', onDragEnd)
-                .on('pointermove', onDragMove_gai);
-            
-
-
-            function onDragStart(event) {
-
-                // var x = event.pageX;
-                // var y = event.pageY;
-                // var loc = getLocation(x,y);
-                // // store a reference to the data
-                // the reason for this is because of multitouch
-                // we want to track the movement of this particular touch
-                this.data = event.data;
-                this.dragging = true;
-                console.log("sdasd")
-            }
-
-            function onDragEnd() {
-                this.dragging = false;
-                // set the interaction data to null
-                this.data = null;
-            }
-
-            // function onDragMove() {
-            //     if (this.dragging) {
-            //         const newPosition = this.data.ISPOINTINPATH(this.parent);
-            //         this.x = newPosition.x;
-            //         this.y = newPosition.y;
-            //         lines.x = newPosition.x;
-            //         lines.y = newPosition.y;
-            //     }
-            // }
-            
-
-            //获取当前鼠标点击的坐标
-            // function getLocation(x, y) {
-            //     var bbox = canvas.getBoundingClientRect();
-            //     return {
-            //         x: (x - bbox.left) * (canvas.width / bbox.width),
-            //         y: (y - bbox.top) * (canvas.height / bbox.height)
-            //     };
-            // }
-
-            app.stage.addChild(circles);
-
-            document.getElementsByTagName("canvas")[0].id = "force_canvas";
-            var canvas = document.getElementsByTagName("canvas");
-            console.log(canvas)
-            var canvas_force = canvas[0];
-            canvas_force.addEventListener('mousedown', onDragStart, false);
-            canvas_force.addEventListener('mouseup', onDragEnd, false);
-            canvas_force.addEventListener('mousemove', onDragMove_gai, false);
-            canvas_force.addEventListener('mouseout', onDragEnd,false);
-            function onDragMove_gai() {
-                if (this.dragging) {
-                    const newPosition = getMousePos(this, event);
-                    circles.x = newPosition.x;
-                    circles.y = newPosition.y;
-                    lines.x = newPosition.x;
-                    lines.y = newPosition.y;
+                    if (delta)
+                        handle(delta);
                 }
-            }
+                //上下滚动时的具体处理函数
+                function handle(delta) {
+                        if (delta <0){//向下滚动
+                            circles.scale.x *= 1.2;
+                            circles.scale.y *= 1.2;
+                            lines.scale.x *= 1.2;
+                            lines.scale.y *= 1.2;
+                        }else{//向上滚动 
+                            circles.scale.x /= 1.2;
+                            circles.scale.y /= 1.2;
+                            lines.scale.x /= 1.2;
+                            lines.scale.y /= 1.2;
+                        }
+                }
+                
 
-            function getMousePos(canvas, event) {
-                //1
-                var rect = canvas.getBoundingClientRect();
-                //2
-                var x = event.clientX - rect.left * (canvas.width / rect.width);
-                var y = event.clientY - rect.top * (canvas.height / rect.height);
-                // console.log("x:"+x+",y:"+y);
-                ans = {}
-                ans.x = x;
-                ans.y = y;
-                return ans;
-            }
-        });
+                circles.buttonMode = true;
+                circles.interactive = true;
+                
+                circles
+                    .on('pointerdown', onDragStart)
+                    .on('pointerup', onDragEnd)
+                    .on('pointerupoutside', onDragEnd)
+                    .on('pointermove', onDragMove_gai);
+                
+
+
+                function onDragStart(event) {
+
+                    // var x = event.pageX;
+                    // var y = event.pageY;
+                    // var loc = getLocation(x,y);
+                    // // store a reference to the data
+                    // the reason for this is because of multitouch
+                    // we want to track the movement of this particular touch
+                    this.data = event.data;
+                    this.dragging = true;
+                    console.log("sdasd")
+                }
+
+                function onDragEnd() {
+                    this.dragging = false;
+                    // set the interaction data to null
+                    this.data = null;
+                }
+
+                // function onDragMove() {
+                //     if (this.dragging) {
+                //         const newPosition = this.data.ISPOINTINPATH(this.parent);
+                //         this.x = newPosition.x;
+                //         this.y = newPosition.y;
+                //         lines.x = newPosition.x;
+                //         lines.y = newPosition.y;
+                //     }
+                // }
+                
+
+                //获取当前鼠标点击的坐标
+                // function getLocation(x, y) {
+                //     var bbox = canvas.getBoundingClientRect();
+                //     return {
+                //         x: (x - bbox.left) * (canvas.width / bbox.width),
+                //         y: (y - bbox.top) * (canvas.height / bbox.height)
+                //     };
+                // }
+
+                app.stage.addChild(circles);
+
+                document.getElementsByTagName("canvas")[0].id = "force_canvas";
+                var canvas = document.getElementsByTagName("canvas");
+                console.log(canvas)
+                var canvas_force = canvas[0];
+                canvas_force.addEventListener('mousedown', onDragStart, false);
+                canvas_force.addEventListener('mouseup', onDragEnd, false);
+                canvas_force.addEventListener('mousemove', onDragMove_gai, false);
+                canvas_force.addEventListener('mouseout', onDragEnd,false);
+                function onDragMove_gai() {
+                    if (this.dragging) {
+                        const newPosition = getMousePos(this, event);
+                        circles.x = newPosition.x;
+                        circles.y = newPosition.y;
+                        lines.x = newPosition.x;
+                        lines.y = newPosition.y;
+                    }
+                }
+
+                function getMousePos(canvas, event) {
+                    //1
+                    var rect = canvas.getBoundingClientRect();
+                    //2
+                    var x = event.clientX - rect.left * (canvas.width / rect.width);
+                    var y = event.clientY - rect.top * (canvas.height / rect.height);
+                    // console.log("x:"+x+",y:"+y);
+                    ans = {}
+                    ans.x = x;
+                    ans.y = y;
+                    return ans;
+                }
+            });
+        })
     })
-
-
+    
 }
-
 
 drawforce();
-
-// setTimeout(canvas_move(), 10000);
-// canvas_move();
-function canvas_move(){
-    var canvas = document.getElementsByTagName("canvas");
-    console.log(canvas)
-    var canvas1 = canvas[0];
-    console.log(canvas1)
-    var canvas2 = document.getElementById("force_canvas");
-    
-    
-    console.log(canvas2)
-    canvas_force.addEventListener('mousedown', onDragStart, false);
-    canvas_force.addEventListener('mouseup', onDragEnd, false);
-    canvas_force.addEventListener('mousemove', onDragMove, false);
-    canvas_force.addEventListener('mouseout', onDragEnd,false);
-}
